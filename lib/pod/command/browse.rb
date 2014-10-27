24a2780cddbd5ac074a0fb6c0bd8e9a9d64b3449
Toshihiro Morimoto <dealforest.net@gmail.com>
2014-10-27 16:00:28 +0900

    Fix for rubocop
    ```
    lib/pod/command/browse.rb:10:25: C: Space inside square brackets detected.
          self.arguments = [ CLAide::Argument.new(%w(NAME), true) ]
                            ^
    lib/pod/command/browse.rb:10:62: C: Space inside square brackets detected.
          self.arguments = [ CLAide::Argument.new(%w(NAME), true) ]
                                                                 ^
    ```

--- lib/pod/command/browse.rb
+++ lib/pod/command/browse.rb
@@ -7,7 +7,7 @@ module Pod
         Opens the homepage of a pod in the browser.
       DESC
 
-      self.arguments = [ CLAide::Argument.new(%w(NAME), true) ]
+      self.arguments = [CLAide::Argument.new(%w(NAME), true)]
 
       def self.options
         [
